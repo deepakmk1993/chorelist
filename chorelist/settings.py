334959b78be13b43360395f59ddf9d3c90d0e6b3
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-10 02:42:30 +0000

    first commit

--- chorelist/settings.py
+++ chorelist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mezvdxg$!coopetd34)kxdw5wu%dr#w9kfskc6z7w)1=32^=gj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com','.c9users.io']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
