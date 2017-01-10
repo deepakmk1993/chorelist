e787293303bca80cbb8ec837e1f80f08f532f491
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-10 06:37:14 +0000

    created models and registered them

--- chores/admin.py
+++ chores/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from .models import ChoreList, Chore
 
 # Register your models here.
+class ChoreInline(admin.TabularInline):
+    model = Chore
+    extra = 2
 
-admin.site.register(ChoreList)
-admin.site.register(Chore)
+class ChoreListAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Date Info', {'fields': ['due_date'], 'classes': ['collapse']})
+        ]
+    inlines = [ChoreInline]
+
+admin.site.register(ChoreList, ChoreListAdmin)
