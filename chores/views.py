4be75863a1f152b8459b2d94cef5743afed75e6b
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-10 10:08:13 +0000

    updated views & urls and added templates

--- chores/views.py
+++ chores/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from .models import ChoreList, Chore
 
 # Create your views here.
 def index(request):
-    return HttpResponse("You're at the ChoreList index. ")
+    # return HttpResponse("You're at the ChoreList index. ")
+    # output = ', '.join([cl.name for cl in lists])
+    # return HttpResponse(output)
+    chorelists = ChoreList.objects.all()
+    template = loader.get_template('chores/index.html')
+    context = RequestContext(request, {
+        'chorelists': chorelists
+    })
+    return HttpResponse(template.render(context))
 
 def detail(request, chorelist_id):
     return HttpResponse("You're looking at ChoreList #%s" % chorelist_id)
